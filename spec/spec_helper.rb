102332c2dc075da6ca8b49f21affc3bd28c05fa1
Ronnie Mose <rm@travelaccommodation.eu>
2016-08-03 12:26:24 +0200

    Add SimpleCov formatting to support local code coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+
+SimpleCov.start do
+  add_filter 'spec'
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ])
+end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
